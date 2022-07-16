730ae5ddee2cd8ecb7379c699acbdd66d4b8712f
mojitha <53862477+mojitha@users.noreply.github.com>
2022-07-16 19:52:15 +0530

    added changes for hosting

--- frontend/src/utils/globals.js
+++ frontend/src/utils/globals.js
@@ -1,11 +1,10 @@
 const appToken = "@DeranaNewsToken";
-const portServer = "5000";
-const baseUrl = `http://localhost:${portServer}/api`;
+const baseUrl = `/api`;
 const apiCategories = `${baseUrl}/categories`;
 const apiUsers = `${baseUrl}/users`;
 const apiNews = `${baseUrl}/news`;
 const apiFiles = `${baseUrl}/files`;
-const publicUrl = `http://localhost:${portServer}/public`;
+const publicUrl = `/public`;
 const uploadsUrl = `${publicUrl}/uploads`;
 
 module.exports = {
